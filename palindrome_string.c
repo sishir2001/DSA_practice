edebd8ddd4dad6df688f7bded6e9c66f5cb7863c
sishir2001 <54372985+sishir2001@users.noreply.github.com>
2020-10-25 18:45:26 +0530

    Add files via upload
    checking palindrome of a string

---
+++ palindrome_string.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+// palindromes in string 
+void check_palindrome(char *s){
+    int i =0;
+    for(;s[i]!='\0';i++){
+
+    }
+    i=i-1;
+    int count =0;
+    for(int j=0;j<i;j++,i--){
+        if(s[j] != s[i]){
+        count =1;
+        printf("%s is not a palindrome\n",s);
+        break;
+        }
+    }
+    if(count == 0)
+    printf("%s is a palindrome\n",s);
+}
+
+void main(void){
+    char s[] = "madam",a[] = "sisihri";
+    check_palindrome(s);
+    check_palindrome(a);
+}
